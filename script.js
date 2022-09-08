b8a4883b2efe0d337b30ce899bacac55a290ff84
Brandon Cozart <brandon.cozart2000@gmail.com>
2022-09-08 04:37:21 -0400

    some jscript work
    repo count added

---
+++ script.js
@@ -0,0 +1,27 @@
+
+let count = document.getElementById('repositoryCount');
+
+function sleep(ms){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(ms);
+
+        }, ms);
+    })
+}
+
+window.onload = async function() {
+
+    const goal = new URL('https://api.github.com/users/braintoobig1/repos');
+    const response = await fetch(goal);
+    const data = await response.json();
+
+
+    for(let i=0; i <= data.length; i++){
+
+        //const counter = data.length;
+        await sleep(500);
+        count.innerHTML = i;
+    }
+    
+  }
