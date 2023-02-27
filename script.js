d0eb75f47ee433495c00d67c930c5d4f59885382
CB <brandon.cozart2000@gmail.com>
2023-02-26 20:23:53 -0500

    Change

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
-
-let count = document.getElementById('repositoryCount');
-
-function sleep(ms){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(ms);
-
-        }, ms);
+$(document).ready(function () {
+    $("#transcript-button").on("click", function(){
+        document.getElementById("reuseable-modal").classList.toggle("hide");
+    });
+    $("#modal-close").on("click", function(){
+        document.getElementById("reuseable-modal").classList.toggle("hide");
     })
-}
+});
 
-window.onload = async function() {
+
+window.onload = async function () {
 
     const goal = new URL('https://api.github.com/users/brandontcozart/repos');
     const response = await fetch(goal);
     const data = await response.json();
 
 
-    for(let i=0; i <= data.length; i++){
+    for (let i = data.length-1; i >= 0; i--) {
+        let newrow = $(`
+    <tr id="row" onclick="window.open('${data[i].html_url}');">
+        <td>
+        ${data[i].name}
+        </td>
 
-        //const counter = data.length;
-        await sleep(500);
-        count.innerHTML = i;
+        <td>
+        ${data[i].description ? data[i].description : "Dungeons and Dragons dungeon master Application"}
+        </td>
+
+        <td> 
+        ${data[i].language ? data[i].language : "Java"}
+        </td>
+
+        <td> 
+        ${new Date(data[i].created_at).toISOString().split('T')[0]}
+        </td>
+    </tr>
+    `)
+
+        $("#table-body").append(newrow);
     }
-    
-  }
+
+}
