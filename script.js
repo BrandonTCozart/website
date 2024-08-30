bb5ea742dd039cb1d26d3aad432c1b1a88ece2b5
Brandon Cozart <brandon.cozart2000@gmail.com>
2024-08-30 17:21:26 -0400

    More work

--- script.js
+++ script.js
@@ -10,33 +10,46 @@ $(document).ready(function () {
 
 window.onload = async function () {
 
-    const goal = new URL('https://api.github.com/users/brandontcozart/repos');
-    const response = await fetch(goal);
-    const data = await response.json();
+    const apiKey = 'AIzaSyBVSXGKQAFnlnW0kUW7j6fqDxPrZzwEy3E';
+    const clientID = '941814250597-hlf4k084up839jc3os6qk0p8ds4odpk9.apps.googleusercontent.com';
+    const scope = 'https://www.googleapis.com/auth/youtube';
 
 
-    for (let i = data.length-1; i >= 0; i--) {
-        let newrow = $(`
-    <tr id="row" onclick="window.open('${data[i].html_url}');">
-        <td>
-        ${data[i].name}
-        </td>
+    gapi.load('client:auth2', function() {
+        gapi.client.init({
+          apiKey: apiKey,
+          clientId: clientID,
+          scope: scope
+        }).then(function() {
+          // API is ready!
+        });
+      });
 
-        <td>
-        ${data[i].description ? data[i].description : "Dungeons and Dragons dungeon master Application"}
-        </td>
+      const videoData = {
+        snippet: {
+          title: 'My New Video',
+          description: 'This is a description of my video.',
+          tags: ['tag1', 'tag2', 'tag3'],
+          categoryId: 1 // Replace with the desired category ID
+        },
+        status: {
+          privacyStatus: 'public' // 'public', 'private', or 'unlisted'
+        }
+      };
 
-        <td> 
-        ${data[i].language ? data[i].language : "Java"}
-        </td>
 
-        <td> 
-        ${new Date(data[i].created_at).toISOString().split('T')[0]}
-        </td>
-    </tr>
-    `)
+      function insert(){
+        gapi.client.youtube.videos.insert({
+            part: 'snippet,status',
+            resource: videoData
+          }).then(function(response) {
+            console.log('Video inserted successfully:', response);
+            // Handle the video insertion response (e.g., get the video ID)
+          }).catch(function(error) {
+            console.error('Error inserting video:', error);
+          });
+      }
 
-        $("#table-body").append(newrow);
-    }
-
+      
+      
 }
